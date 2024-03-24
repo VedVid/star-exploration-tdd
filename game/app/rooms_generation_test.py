e07ba2dbdbf145b5429810365f62bfa44a6134fc
Tomasz Nowakowski <toalak@gmail.com>
2024-03-24 13:02:15 +0100

    Added tests for generating two and three rooms at once

--- game/app/rooms_generation_test.py
+++ game/app/rooms_generation_test.py
@@ -20,3 +20,19 @@ def test__should_generate_one_room__when_only_one_room_should_be_generated():
     result = rooms_generator.generate_rooms(1)
 
     assert len(result) == 1
+
+
+def test__should_generate_two_rooms__when_two_rooms_should_be_generated():
+    rooms_generator = RoomsGenerator()
+
+    result = rooms_generator.generate_rooms(2)
+
+    assert len(result) == 2
+
+
+def test__should_generate_three_rooms__when_three_rooms_should_be_generated():
+    rooms_generator = RoomsGenerator()
+
+    result = rooms_generator.generate_rooms(3)
+
+    assert len(result) == 3
