400abcc6ea58448dfef6423dceacee43415832d7
Tomasz Nowakowski <toalak@gmail.com>
2024-03-24 15:57:18 +0100

    Make the test pass; no refactoring needed

--- game/app/ship.py
+++ game/app/ship.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
+from ship_variables import *
+
+
 class Ship:
-    pass
+    def __init__(self, attack):
+        self.attack = attack
+        if self.attack < ATTACK_MIN:
+            self.attack = ATTACK_MIN
