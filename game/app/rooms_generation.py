b17d15e0de3fc683fe32a02c8c6bf64fea86db85
Tomasz Nowakowski <toalak@gmail.com>
2024-03-24 13:45:07 +0100

    Docstrings added to the RoomsGenerator

--- game/app/rooms_generation.py
+++ game/app/rooms_generation.py
@@ -7,13 +7,43 @@ from room_types import *
 
 
 class RoomsGenerator:
+    """
+    RoomsGenerator is a class that is used for generating rooms. These rooms are available for the player
+    to choose from to continue their journey.
+
+    Attributes
+    ----------
+    _rooms : list of strings
+        Contains every possible type of room to generate.
+    """
 
     _rooms = [SPACE_STATION, PLANET_BASE, SPACE]
 
     def choose_room_randomly(self):
+        """
+        Returns random room from _rooms class list. It is used by generate_rooms method.
+
+        Returns
+        -------
+        room : string
+            Randomly chosen room from _rooms class attribute.
+        """
         return choice(self._rooms)
 
     def generate_rooms(self, amount):
+        """
+        Returns list of unique rooms.
+
+        Parameters
+        ----------
+        amount : int
+            Amount of rooms that should be generated.
+
+        Returns
+        -------
+        new_rooms: list of strings
+            List of generated rooms.
+        """
         new_rooms = []
         while amount > 0:
             new_room = self.choose_room_randomly()
