e44f3e5e30735ecde0c43b804d86d6ae5a66812d
Tomasz Nowakowski <toalak@gmail.com>
2024-03-24 16:06:13 +0100

    Make the test pass

--- game/app/ship.py
+++ game/app/ship.py
@@ -5,9 +5,10 @@ from ship_variables import *
 
 
 class Ship:
-    def __init__(self, attack):
+    def __init__(self, attack=ATTACK_MIN, defense=DEFENSE_MIN):
         self.attack = attack
         if self.attack < ATTACK_MIN:
             self.attack = ATTACK_MIN
         elif self.attack > ATTACK_MAX:
             self.attack = ATTACK_MAX
+        self.defense = defense
