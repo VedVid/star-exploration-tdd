71dab570ff60a60a469f810627cf0b57e418d8df
Tomasz Nowakowski <toalak@gmail.com>
2024-03-24 16:09:32 +0100

    Failing test for defense set too high

--- game/app/ship_test.py
+++ game/app/ship_test.py
@@ -31,6 +31,12 @@ def test__should_set_defense_to_min__when_attack_set_below_min_value():
     assert ship.defense == DEFENSE_MIN
 
 
+def test__should_set_defense_to_max__when_defense_set_over_max_value():
+    ship = Ship(defense=DEFENSE_MAX+1)
+
+    assert ship.defense == DEFENSE_MAX
+
+
 def test__should_not_interfere_to_defense_value__when_defense_is_in_range():
     ship = Ship(attack=ATTACK_MIN, defense=DEFENSE_MIN)
 
