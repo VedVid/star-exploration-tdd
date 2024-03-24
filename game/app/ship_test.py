1ba63353d42f2de5292c80f40948d1fcd4318d4b
Tomasz Nowakowski <toalak@gmail.com>
2024-03-24 16:00:47 +0100

    Passing test for attack value withing ranges

--- game/app/ship_test.py
+++ game/app/ship_test.py
@@ -17,3 +17,9 @@ def test__should_set_ship_attack_to_max__when_attack_set_over_max_value():
     ship = Ship(attack=ATTACK_MAX+1)
 
     assert ship.attack == ATTACK_MAX
+
+
+def test__should_not_interfere_to_attack_value__when_attack_is_in_range():
+    ship = Ship(attack=ATTACK_MIN+1)
+
+    assert ship.attack == ATTACK_MIN + 1
