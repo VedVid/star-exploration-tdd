ec848e34c0c2c381d4bcf0d406983303fef12968
Tomasz Nowakowski <toalak@gmail.com>
2024-03-24 12:51:47 +0100

    Refactoring: added a couple types of rooms, and room generator returns random room

---
+++ game/app/room_types.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
+SPACE_STATION = "space station"
+PLANET_BASE = "planet base"
+SPACE = "space"
--- game/app/rooms_generation.py
+++ game/app/rooms_generation.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
 
+from random import choice
+
+from room_types import *
+
+
 class RoomsGenerator:
 
+    _rooms = [SPACE_STATION, PLANET_BASE, SPACE]
+
     def generate(self):
-        return "room"
+        return choice(self._rooms)
