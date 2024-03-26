a57fa9e1295da16acf9bd8fb431c9ae22f80daf8
Tomasz Nowakowski <toalak@gmail.com>
2024-03-26 22:05:48 +0100

    Show welcome message

--- game/main.py
+++ game/main.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
 
+from app.rooms_generation import RoomsGenerator
+
+
 if __name__ == "__main__":
-    print("Hello World!")
+    rooms_generator = RoomsGenerator()
+    print("You boarded your brand-new Explorer-class starship.\nWhere will you go?")
