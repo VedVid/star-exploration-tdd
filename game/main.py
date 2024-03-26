85974d5e82958ee879665a95adf6d9a52346f893
Tomasz Nowakowski <toalak@gmail.com>
2024-03-26 22:10:12 +0100

    Print hardcoded game menu

--- game/main.py
+++ game/main.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
 
+from random import randint
+
 from app.rooms_generation import RoomsGenerator
 
 
 if __name__ == "__main__":
     rooms_generator = RoomsGenerator()
-    print("You boarded your brand-new Explorer-class starship.\nWhere will you go?")
+    print("\n*********\n")
+    print("You boarded your brand-new Explorer-class starship.\nWhere will you go?\n")
+    rooms = rooms_generator.generate_rooms(randint(1, 3))
+    for i, room in enumerate(rooms):
+        print(f"{i+1}) {room}")
