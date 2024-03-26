b254691ffe4ed338b4da76aa432702634e27e650
Tomasz Nowakowski <toalak@gmail.com>
2024-03-26 22:54:37 +0100

    Failing test for setting options

--- game/app/menus/base_menu_test.py
+++ game/app/menus/base_menu_test.py
@@ -11,3 +11,11 @@ def test__should_raise_exception__when_empty_options_accessed():
 
     with pytest.raises(ValueError, match="Non-empty list of options expected."):
         menu.print_options()
+
+
+def test__should_set_options__when_setter_for_options_called():
+    menu = BaseMenu()
+
+    menu.set_options("aaaaa", "bbbbb", "ccccc")
+
+    assert len(menu.options) == 3
