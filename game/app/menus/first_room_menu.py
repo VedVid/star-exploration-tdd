133f89b521d666b87854145f579407bcfae77a24
Tomasz Nowakowski <toalak@gmail.com>
2024-03-28 23:15:23 +0100

    Making the test pass

--- game/app/menus/first_room_menu.py
+++ game/app/menus/first_room_menu.py
@@ -5,4 +5,8 @@ from .base_menu import BaseMenu
 
 
 class FirstRoomMenu(BaseMenu):
-    pass
+
+    def handle_option(self, option_chosen):
+        match option_chosen:
+            case "1":
+                return self.options[0]
