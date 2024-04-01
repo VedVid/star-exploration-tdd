dc462c369543ea063103104851f37cc084b707cf
Tomasz Nowakowski <toalak@gmail.com>
2024-04-01 23:02:37 +0200

    Major refactor; most likely I should create more tests for this _before_ I typed all this code...

--- game/app/room.py
+++ game/app/room.py
@@ -3,18 +3,21 @@
 
 import random
 
+from .cargo_types import ALL_CARGO_TYPES
 from .room_types import ALL_ROOM_TYPES
 
 
 class Room:
-    def __init__(self, room_type=None, doors=None):
+    def __init__(self, room_type=None, doors=None, cargo_list=None):
         self.room_type = room_type
         if self.room_type is None:
             self.set_room_type()
         self.doors = doors
         if self.doors is None:
             self.set_doors()
-        self.cargo_list = [1]
+        self.cargo_list = cargo_list
+        if self.cargo_list is None:
+            self.set_cargo_list()
 
     def get_room_type(self):
         return self.room_type
@@ -35,3 +38,21 @@ class Room:
 
     def get_doors(self):
         return self.doors
+
+    def set_cargo_list(self, cargo_list="random"):
+        try:
+            for cargo in cargo_list:
+                self.cargo_list.append(cargo)
+        except AttributeError:
+            cargo_num = random.randint(1, len(ALL_CARGO_TYPES))
+            tmp_cargo_types = ALL_CARGO_TYPES[:]
+            self.cargo_list = []
+            while len(self.cargo_list) < cargo_num:
+                random.shuffle(tmp_cargo_types)
+                new_cargo = tmp_cargo_types.pop()
+                print(new_cargo)
+                new_cargo["price_current"] = random.randint(
+                    new_cargo["price_min"],
+                    new_cargo["price_max"]
+                )
+                self.cargo_list.append(new_cargo)
