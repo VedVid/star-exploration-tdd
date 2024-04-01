86624ff6f2aa239e7fffd2a507a47a4f2bbf0e77
Tomasz Nowakowski <toalak@gmail.com>
2024-04-01 23:52:03 +0200

    Docstrings to the Room class added

--- game/app/room.py
+++ game/app/room.py
@@ -8,6 +8,33 @@ from .room_types import ALL_ROOM_TYPES
 
 
 class Room:
+    """
+    Room is a class that keeps all data related to the room in which player currently is, like room type, list of
+    rooms that are available from there, and cargo available to buy here. Data from Room is used by various
+    kind of menus. Room class does not define any behaviour with exception for setters and getters that are not quite
+    necessary, but helpful for testing.
+
+    Parameters
+    ----------
+    room_type : Union[str | None]
+        Type of the current room. It should match one of the values definied in room_types.py file. If set to None
+        as it is by default, room type is chosen randomly.
+    doors : Union[list | None]
+        List of all rooms available from this room. These rooms are not generated yet, only their types are specified.
+        Every room must match one of the values definied in room_types.py file.
+        If set to None as it is by default, game chooses amount of doors and their types randomly.
+    cargo_list : Union[list | None]
+        List of cargo available there. Every cargo should match one of the values definied in cargo_types.py file.
+        If set to None as it is by default, game chooses amount of cargo and their type randomly.
+
+    Attributes
+    ----------
+    header : str
+        Header of the menu. Will be printed on the top, in all-caps.
+    options : Union[list | None]
+        List of options that will be printed (and handled by the children of this class).
+    """
+
     def __init__(self, room_type=None, doors=None, cargo_list=None):
         self.room_type = room_type
         if self.room_type is None:
@@ -20,9 +47,14 @@ class Room:
             self.set_cargo_list()
 
     def get_room_type(self):
+        """Returns type of the current room."""
         return self.room_type
 
     def set_room_type(self, room_type="random"):
+        """
+        Sets type of the current room. If "random" or invalid value is passed to this method, the room type
+        is chosen randomly.
+        """
         if room_type in ALL_ROOM_TYPES:
             self.room_type = room_type
         else:
@@ -30,9 +62,14 @@ class Room:
             self.room_type = random.choice(ALL_ROOM_TYPES)
 
     def get_doors(self):
+        """Returns rooms that are avaible to get there from the current room."""
         return self.doors
 
     def set_doors(self, doors="random"):
+        """
+        Sets passageways to another rooms. If "random" or invalid value is passed to this method, the doors
+        are being set randomly.
+        """
         invalid = False
         if isinstance(doors, list):
             for door in doors:
@@ -48,9 +85,14 @@ class Room:
                 self.doors.append(random.choice(ALL_ROOM_TYPES))
 
     def get_cargo_list(self):
+        """Returns list of cargo that is available for player to purchase."""
         return self.cargo_list
 
     def set_cargo_list(self, cargo_list="random"):
+        """
+        Sets list of cargo available for player to purchase.. If "random" or invalid value is passed to this method,
+        the cargo list is created randomly.
+        """
         if isinstance(cargo_list, list):
             self.cargo_list = []
             for cargo in cargo_list:
