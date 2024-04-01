509f7a32faa354c15753916acb8391460d0b7151
Tomasz Nowakowski <toalak@gmail.com>
2024-04-01 13:24:36 +0200

    Additional tests added; does it violate TDD paradigm?

--- game/app/room_test.py
+++ game/app/room_test.py
@@ -19,3 +19,22 @@ def test__should_return_correct_room_type__when_get_room_type_called():
     assert result_space_station == SPACE_STATION
     assert result_planet_base == PLANET_BASE
     assert result_space == SPACE
+
+
+def test__should_set_correct_room_type__when_set_room_type_called_with_correct_room_type():
+    room_space_station = Room()
+    room_space_station.set_room_type(SPACE_STATION)
+    room_planet_base = Room()
+    room_planet_base.set_room_type(PLANET_BASE)
+    room_space = Room()
+    room_space.set_room_type(SPACE)
+
+    assert room_space_station.room_type == SPACE_STATION
+    assert room_planet_base.room_type == PLANET_BASE
+    assert room_space.room_type == SPACE
+
+
+def test__should_set_random_room_type__when_room_is_instanced_without_room_type_argument():
+    random_room = Room()
+
+    assert random_room.room_type in ALL_ROOM_TYPES
