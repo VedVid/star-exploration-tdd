0d11309277e55be118a14a9392193b7c71f90f3e
Tomasz Nowakowski <toalak@gmail.com>
2024-04-01 13:57:38 +0200

    Refactor

--- game/app/room.py
+++ game/app/room.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
 
-from random import choice
+import random
 
 from .room_types import ALL_ROOM_TYPES
 
 
 class Room:
-    def __init__(self, room_type=None):
+    def __init__(self, room_type=None, doors=None):
         self.room_type = room_type
         if self.room_type is None:
             self.set_room_type()
+        self.doors = doors
+        if self.doors is None:
+            self.set_doors()
 
     def get_room_type(self):
         return self.room_type
@@ -20,7 +23,14 @@ class Room:
             self.room_type = room_type
         else:
             # Assuming room_type == "random"
-            self.room_type = choice(ALL_ROOM_TYPES)
+            self.room_type = random.choice(ALL_ROOM_TYPES)
+
+    def set_doors(self, doors="random"):
+        if doors == "random":
+            no_of_doors = random.randint(1, len(ALL_ROOM_TYPES))
+            self.doors = []
+            while len(self.doors) < no_of_doors:
+                self.doors.append(random.choice(ALL_ROOM_TYPES))
 
     def get_doors(self):
-        return [1]
+        return self.doors
