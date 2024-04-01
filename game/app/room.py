49b062d71efe55cbbf94c2e6f4eb24b3a937e7e1
Tomasz Nowakowski <toalak@gmail.com>
2024-04-01 13:18:29 +0200

    Refactor 2 – added set_room_type function

--- game/app/room.py
+++ game/app/room.py
@@ -10,7 +10,14 @@ class Room:
     def __init__(self, room_type=None):
         self.room_type = room_type
         if self.room_type is None:
-            self.room_type = choice(ALL_ROOM_TYPES)
+            self.set_room_type()
 
     def get_room_type(self):
         return self.room_type
+
+    def set_room_type(self, room_type="random"):
+        if room_type in ALL_ROOM_TYPES:
+            self.room_type = room_type
+        else:
+            # Assuming room_type == "random"
+            self.room_type = choice(ALL_ROOM_TYPES)
