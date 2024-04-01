898237bcfbe67dee166fd67277b9dad609b31ea5
Tomasz Nowakowski <toalak@gmail.com>
2024-04-01 13:14:27 +0200

    Refactored anyway

--- game/app/room.py
+++ game/app/room.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
 
+from random import choice
+
+from .room_types import ALL_ROOM_TYPES
+
+
 class Room:
-    def __init__(self, room_type):
+    def __init__(self, room_type=None):
         self.room_type = room_type
+        if self.room_type is None:
+            self.room_type = choice(ALL_ROOM_TYPES)
 
     def get_room_type(self):
         return self.room_type
--- game/app/room_types.py
+++ game/app/room_types.py
@@ -4,3 +4,5 @@
 SPACE_STATION = "space station"
 PLANET_BASE = "planet base"
 SPACE = "space"
+
+ALL_ROOM_TYPES = [SPACE_STATION, PLANET_BASE, SPACE]
