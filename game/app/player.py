d15d5f3ef94f7d9fa40d394c02e9a402fc6cde88
Tomasz Nowakowski <toalak@gmail.com>
2024-04-02 23:33:19 +0200

    Docstring updated with more info about player's ship

--- game/app/player.py
+++ game/app/player.py
@@ -18,7 +18,8 @@ class Player:
     Attributes
     ----------
     ship : Ship
-        Player's ship, with its cargo space, hp, attack and defense.
+        Player's ship, with its cargo space, hp, attack and defense. Since player won't change the ship during
+        the gameplay, there is no setter method for setting ship.
     room : Union[Room | None]
         When Player is spawned at the beginning of game, then room is set to None. It gets set to Room intance
         only after player chooses the first destination of their travel.
