9ed43e6b9851f1fca69e29b02afe1c4a71d4c6c2
Tomasz Nowakowski <toalak@gmail.com>
2024-04-02 22:33:10 +0200

    Failing test for setting user room

--- game/app/player_test.py
+++ game/app/player_test.py
@@ -4,6 +4,7 @@
 import pytest
 
 from .player import Player
+from .room_types import *
 
 
 def test__should_player_location_be_set_as_none__when_game_starts():
@@ -12,3 +13,12 @@ def test__should_player_location_be_set_as_none__when_game_starts():
     result = player.room
 
     assert result is None
+
+
+def test__should_set_player_room__when_player_travels():
+    player = Player()
+
+    player.set_room(PLANET_BASE)
+    result = player.room
+
+    assert result == PLANET_BASE
