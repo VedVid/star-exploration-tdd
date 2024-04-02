b70de3bd556424d331c49bd8a86e72de5c09d0a4
Tomasz Nowakowski <toalak@gmail.com>
2024-04-02 22:50:23 +0200

    Two unnecessary tests removed; getter test for room fixed

--- game/app/player_test.py
+++ game/app/player_test.py
@@ -26,24 +26,10 @@ def test__should_set_player_room__when_player_travels():
     assert result.room_type == PLANET_BASE
 
 
-def test__should_raise_exception__when_incorrect_type_of_argument_passed_to_room_setter():
-    player = Player()
-
-    with pytest.raises(ValueError, match="Incorrect room type set."):
-        player.set_room([12325])
-
-
-def test__should_raise_exception__when_incorrect_room_string_passed_to_room_setter():
-    player = Player()
-
-    with pytest.raises(ValueError, match="Incorrect room type set."):
-        player.set_room("something")
-
-
 def test__should_return_player_room__when_getter_is_called():
     player = Player()
 
     player.set_room(PLANET_BASE)
     result = player.get_room()
 
-    assert result == PLANET_BASE
+    assert result.room_type == PLANET_BASE
