9cf415265b4fe633d362a6471391145f06b850e1
Tomasz Nowakowski <toalak@gmail.com>
2024-04-03 20:05:10 +0200

    Failing test for selecting first option ("Travel to...") in the Room Menu

---
+++ game/app/menus/room_menu_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+import pytest
+
+from .room_menu import RoomMenu
+from ..room import Room
+from ..room_types import *
+
+
+def test__should_return_list_of_travel_destinations__when_player_choose_first_option(monkeypatch):
+    room = Room(doors=[SPACE_STATION, PLANET_BASE])
+    menu = RoomMenu(header=room.room_type)
+
+    monkeypatch.setattr("builtins.input", lambda _: "1")
+    result = menu.handle_option(menu.take_input())
+
+    assert result == [SPACE_STATION, PLANET_BASE]
