0cb0cf773383595bbb8f06c6f3a3a8e4ec88da95
Tomasz Nowakowski <toalak@gmail.com>
2024-04-03 20:19:07 +0200

    Refactor

--- game/app/menus/room_menu.py
+++ game/app/menus/room_menu.py
@@ -5,5 +5,19 @@ from .base_menu import BaseMenu
 
 
 class RoomMenu(BaseMenu):
+    def __init__(self, header=""):
+        super().__init__(header, options=["Travel to destination", "Buy cargo", "Sell cargo"])
+
     def handle_option(self, option_chosen):
-        return ["space station", "planet base"]
+        option_returned = None
+        try:
+            option_chosen_int = int(option_chosen)
+            if option_chosen_int < 1:
+                option_returned = None
+            else:
+                option_returned = self.options[int(option_chosen) - 1]
+        except IndexError or ValueError:
+            option_returned = None
+        finally:
+            if option_returned == "Travel to destination":
+                return ["space station", "planet base"]
