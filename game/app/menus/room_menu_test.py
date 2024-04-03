e919a6cb169252086869c92b20b30a70e04c5b80
Tomasz Nowakowski <toalak@gmail.com>
2024-04-03 20:41:36 +0200

    Added missing test for handle_input_taken to improve coverage

--- game/app/menus/room_menu_test.py
+++ game/app/menus/room_menu_test.py
@@ -16,3 +16,41 @@ def test__should_return_list_of_travel_destinations__when_player_choose_first_op
     result = menu.handle_input_taken(menu.take_input())
 
     assert result == [SPACE_STATION, PLANET_BASE]
+
+
+def test__should_return_none__when_input_is_number_lesser_than_one(monkeypatch):
+    """Ensure that handle_input_taken method returns None on incorrect number entered by user."""
+
+    room = Room()
+    menu = RoomMenu(room=room)
+
+    monkeypatch.setattr("builtins.input", lambda _: "0")
+    result = menu.handle_input_taken(menu.take_input())
+
+    assert result is None
+
+
+def test__should_return_none__when_chosen_option_is_incorrect(monkeypatch):
+    """Ensure that handle_input_taken method returns None when incorrect option is chosen."""
+
+    room = Room()
+    menu = RoomMenu(room=room)
+
+    menu.set_options([SPACE_STATION, PLANET_BASE, SPACE])
+    monkeypatch.setattr("builtins.input", lambda _: "999")
+    result = menu.handle_input_taken(menu.take_input())
+
+    assert result is None
+
+
+def test__should_return_none__when_input_is_incorrect(monkeypatch):
+    """Ensure that handle_input_taken method returns None on incorrect type of user input."""
+
+    room = Room()
+    menu = RoomMenu(room=room)
+
+    menu.set_options([SPACE_STATION, PLANET_BASE, SPACE])
+    monkeypatch.setattr("builtins.input", lambda _: "sadafgsdgsf")
+    result = menu.handle_input_taken(menu.take_input())
+
+    assert result is None
