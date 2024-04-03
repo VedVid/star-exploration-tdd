efd1c319913fe63a41531936b6f1c80c32c1b902
Tomasz Nowakowski <toalak@gmail.com>
2024-04-03 20:26:50 +0200

    Fixed Room menu

--- game/app/menus/room_menu.py
+++ game/app/menus/room_menu.py
@@ -9,14 +9,14 @@ class RoomMenu(BaseMenu):
         self.room = room
         super().__init__(self.room.room_type, options=["Travel to destination", "Buy cargo", "Sell cargo"])
 
-    def handle_option(self, option_chosen):
+    def handle_input_taken(self, input_taken):
         option_returned = None
         try:
-            option_chosen_int = int(option_chosen)
+            option_chosen_int = int(input_taken)
             if option_chosen_int < 1:
                 option_returned = None
             else:
-                option_returned = self.options[int(option_chosen) - 1]
+                option_returned = self.options[int(input_taken) - 1]
         except IndexError or ValueError:
             option_returned = None
         finally:
