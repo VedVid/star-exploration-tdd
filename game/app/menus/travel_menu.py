0f829a9714e2f62a10422f5b09badc05a7878ea3
Tomasz Nowakowski <toalak@gmail.com>
2024-04-05 20:46:54 +0200

    Refactoring

--- game/app/menus/travel_menu.py
+++ game/app/menus/travel_menu.py
@@ -10,4 +10,14 @@ class TravelMenu(BaseMenu):
         super().__init__(self.room.room_type, options=self.room.doors)
 
     def handle_input_taken(self, input_taken):
-        return "space station"
+        option_returned = None
+        try:
+            option_chosen_int = int(input_taken)
+            if option_chosen_int < 1:
+                option_returned = None
+            else:
+                option_returned = self.options[int(input_taken) - 1]
+        except IndexError or ValueError:
+            option_returned = None
+        finally:
+            return option_returned
