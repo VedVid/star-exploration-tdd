bad1c68c0a9ea0a1b42cff45455fb0ab58bf7526
Tomasz Nowakowski <toalak@gmail.com>
2024-04-05 20:37:37 +0200

    Failing test for getting door type chosen

--- game/app/menus/travel_menu_test.py
+++ game/app/menus/travel_menu_test.py
@@ -2,3 +2,17 @@
 
 
 import pytest
+
+from ..room import Room
+from ..room_types import *
+from .travel_menu import TravelMenu
+
+
+def test__should_return_destination__when_a_door_is_chosen(monkeypatch):
+    room = Room(doors=ALL_ROOM_TYPES)
+    menu = TravelMenu(room)
+
+    monkeypatch.setattr("builtins.input", lambda _: "1")
+    result = menu.handle_input_taken(menu.take_input())
+
+    assert result == ALL_ROOM_TYPES[0]
